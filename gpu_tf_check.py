1e140f60fc16954613004da5116b948b256b4201
igor71 <34439207+igor71@users.noreply.github.com>
2018-08-01 09:21:11 +0300

    Update gpu_tf_check.py

--- gpu_tf_check.py
+++ gpu_tf_check.py
@@ -4,4 +4,4 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session(config=config)
-print(sess.run(hello)
+print(sess.run(hello))
