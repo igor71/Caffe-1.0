7cf5bd53c9ab75bf82b5aa643fbcca8c448695aa
igor71 <34439207+igor71@users.noreply.github.com>
2019-11-06 15:12:34 +0200

    Update check_tf_installation.sh

--- Manual/check_tf_installation.sh
+++ Manual/check_tf_installation.sh
@@ -2,8 +2,12 @@
 
 ## Verify Tensorflow Installation
 
+rm -f /tmp/*.c
+
 export PY=python3.5
 
 echo "Installed TF version: `${PY} -c "import tensorflow as tf; print(tf.__version__)"`"
+echo "################### Checking Server Configaration For Tensorflow ################"
 ${PY} -c "import tensorflow as tf; print(tf.contrib.eager.num_gpus())"
+echo "#################################################################################"
 echo " Checking TF Computation Abilities: `${PY} -c "import tensorflow as tf;print(tf.reduce_sum(tf.random.normal([1000, 1000])))"`"
